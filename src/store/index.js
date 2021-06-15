0f8f6b369074cd35ee8d6965c93767403c161c79
Luis Yago Fernández <luisyago.fernandez@stratesys-ts.com>
2021-06-15 15:46:22 +0200

    ADD: first contact - VUE.js

--- src/store/index.js
+++ src/store/index.js
@@ -5,10 +5,22 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    userName: "Luis Yago",
+    userSurname: "Fernández",
+    number: 0
   },
   mutations: {
+    acumular(state, payload) {
+      state.number = this.state.number + payload
+    }
   },
   actions: {
+    // acumular(context) {
+    //   context.commit("acumular")
+    // },
+    acumular({ commit }, num) {
+      commit("acumular", num)
+    }
   },
   modules: {
   }
---
+++ src/views/Settings.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="settings">
+    <Title msg="Settings" />
+    <h3>{{ userName }}</h3>
+    <h4>{{ userSurname }}</h4>
+    <hr />
+    <h4>Número de likes:</h4>
+    <div class="contador">
+      <button @click="acumular(-1)">-</button>
+      <p>{{ number }}</p>
+      <button @click="acumular(+1)">+</button>
+    </div>
+  </div>
+</template>
+
+<script>
+import Title from "@/components/Title.vue";
+import { mapState, mapActions } from "vuex";
+
+export default {
+  name: "Settings",
+  components: {
+    Title,
+  },
+  computed: {
+    ...mapState([
+      "userName", "userSurname", "number"
+    ])
+  },
+  methods: {
+    ...mapActions([
+      "acumular"
+    ])
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+  .contador {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+</style>
